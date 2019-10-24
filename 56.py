38a660c6f4f2555888b208e42fa2855ae5cb8757
2i99i5 <zakirovra@gmail.com>
2019-10-24 22:04:36 +0400

    6,7 задачи выполнены.

--- 56.py
+++ 56.py
@@ -10,4 +10,20 @@
 Сформировать словарь, содержащий название предмета и общее количество занятий по нему.
 Вывести словарь на экран.
 """
-
+try:
+    subject = dict()
+    with open("56_txt.txt") as file:
+        for line in file:
+            arr_line = line.replace("\n","").split(":")  # удаляем перенос строки, создаем список
+            arr_line[1] = arr_line[1].split(",")
+            quantity = 0
+            for ed_type in arr_line[1]:
+                ed_type = ed_type.split("-")
+                if ed_type[1].isdigit():
+                    quantity += int(ed_type[1])  # суммируем кол-во уроков
+            subject[arr_line[0]] = quantity  # заполняем словарь
+        print(subject)
+except IOError as e:
+    print(e)
+finally:
+    print("работа с файлом завершена")
